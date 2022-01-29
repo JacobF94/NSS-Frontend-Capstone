38f1af8c39d40b730804e5df466af2b6f94d121c
JacobF94 <jacob.frensley@gmail.com>
2022-01-29 11:58:25 -0600

    initial modules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DailyFitness from './Components/DailyFitness';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <DailyFitness />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
